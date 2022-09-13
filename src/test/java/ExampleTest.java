140d45e904f5ac2d9a6651dbc286d2f2b75da27b
OrlovAleksey <aorlov@sberhealth.ru>
2022-09-13 17:55:42 +0300

    Make tests

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    void exampleTest1(){
+        Assertions.assertTrue(1<3);
+    }
+
+    @Test
+    void exampleTest2(){
+        Assertions.assertTrue(1<5);
+    }
+}
